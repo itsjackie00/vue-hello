1e58b340f6f2811ddc2d2991fe4242f435ff5735
itsjackie00 <jackiedanso33@gmail.com>
2024-03-27 15:07:06 +0100

    added message vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-alert('hello');
+// alert('hello');
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: '',
+            image: 'https://picsum.photos/300/200',
+            altimage: 'random image',
+        }
+
+    },
+}).mount('#message')
